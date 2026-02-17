ca5ea6dd3bfe8a6491fb026a0f6329d7dab87b59
Mohd Zaid <monis.z.786@gmail.com>
2026-02-18 00:50:21 +0800

    Build System & Gradle Modernization

--- app/src/main/java/zaidstudios/wally/Activities/MainActivity.java
+++ app/src/main/java/zaidstudios/wally/Activities/MainActivity.java
@@ -223,15 +223,10 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings: {
-                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
-                finish();
-                break;
-            }
+        if (item.getItemId() == R.id.action_settings) {
+            startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            finish();
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
